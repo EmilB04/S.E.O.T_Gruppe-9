b63cd4dbe5cb6ed624b6a411bb684eabbb576e8e
Andolaus <andolaus@gmail.com>
2024-11-07 15:36:54 +0100

    Fikk et problem med HttpHandler, fikset nå. Laget testklasser

--- api/src/main/java/HttpHandler.java
+++ api/src/main/java/HttpHandler.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,13 +33,16 @@ public class HttpHandler {
         StringBuilder data = new StringBuilder(); 
 
         try {
-            URL url = new URL(urlInput); 
+            URI uri = new URI(urlInput);
+            URL url = uri.toURL(); 
+
+
             HttpURLConnection connection = (HttpURLConnection) url.openConnection(); 
             connection.setRequestMethod("GET");
-
             logger.info("Koblet til: {}", urlInput);
 
             BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
             String infoLine;
 
              while ((infoLine = in.readLine()) != null) {
